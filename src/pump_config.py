526a5dc5afa6d103ccf7333a16a5847e77513bd7
Yulay77 <t_serayet@etu-webschoolfactory.fr>
2024-10-08 18:29:16 +0200

    variables plus en dur

--- src/pump_config.py
+++ src/pump_config.py
@@ -4,20 +4,20 @@ class PumpConfig:
         self.insulin_to_carb_ratio = insulin_to_carb_ratio  # Ratio insuline/glucides
         self.insulin_sensitivity_factor = insulin_sensitivity_factor  # Sensibilité à l'insuline
         self.max_bolus = max_bolus  # Dose maximale de bolus
+        
     
     def validate_basal_rates(self):
         # Lissage des variations abruptes dans les taux basaux
         for i in range(1, len(self.basal_rates)):
             if abs(self.basal_rates[i] - self.basal_rates[i-1]) > 0.3:  # Par exemple, limite à 0,3 U/h
                 self.basal_rates[i] = (self.basal_rates[i] + self.basal_rates[i-1]) / 2  # Lissage
-    def adjust_for_mode(self, mode):
-        if mode == "Sport":
-            # Réduction de 20 % des taux basaux
-            self.basal_rates = [rate * 0.8 for rate in self.basal_rates]
-        elif mode == "Nuit":
-            # Réduction de 15 % des taux basaux
-            self.basal_rates = [rate * 0.85 for rate in self.basal_rates]
-        elif mode == "Jour":
-            # Augmentation de 10 % des taux basaux pendant la journée (6h - 22h)
-            for i in range(6, 22):
-                self.basal_rates[i] *= 1.1
+                
+                
+    def adjust_for_mode(self, mode, adjustments):
+        if mode in adjustments:
+            adjustment = adjustments[mode]
+            if isinstance(adjustment, list):
+                for i in range(len(self.basal_rates)):
+                    self.basal_rates[i] *= adjustment[i]
+            else:
+                self.basal_rates = [rate * adjustment for rate in self.basal_rates]
